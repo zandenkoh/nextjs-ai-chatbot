eb52a41c202209e8ead5b48b16cefd8079667630
Zanden Koh <137964632+zandenkoh@users.noreply.github.com>
2025-07-14 11:48:41 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { SessionProvider } from 'next-auth/react';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  title: 'Zeta AI',
+  description: 'Zeta is a free AI assistant designed by ZLabs to maximize truth and objectivity. Zeta offers real-time search, image generation, trend analysis, and more.',
 };
 
 export const viewport = {
